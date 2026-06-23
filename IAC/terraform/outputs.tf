4a85a17e1973fd89b05d24700dad10fe543328e7
le blond erwan <erwanleblond@gmail.com>
2026-06-23 13:35:24 +0200

    add url for client

--- IAC/terraform/outputs.tf
+++ IAC/terraform/outputs.tf
@@ -18,6 +18,11 @@ output "docs_url" {
   value       = "http://${aws_eip.rag_server.public_ip}:${var.api_port}/docs"
 }
 
+output "client_url" {
+  description = "Client url"
+  value       = "http://${aws_eip.rag_server.public_ip}:8080/client_demo.htm"
+}
+
 output "ssh_command" {
   description = "SSH command to connect to the instance"
   value       = "ssh -i ${var.ssh_private_key_path} ubuntu@${aws_eip.rag_server.public_ip}"
